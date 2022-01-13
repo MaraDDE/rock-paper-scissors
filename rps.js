76cc83c5e2e49658c3a27ec3a8673efe9776181f
MaraDDE <maradediegoesteva@gmail.com>
2022-01-13 17:29:16 -0600

    finished rps.js logic

--- rps.js
+++ rps.js
@@ -1,7 +1,23 @@
-console.log("Hello world!");
+// console.log("Hello world!");
 const options = ['rock', 'paper', 'scissors'];
+let playerScore = 0,
+    computerScore =0,
+    win = 0,
+    lose =0.
+    tie =0;
 
-// RANDOMNLY GENERATE THE COMPUTER'S SELECTION
+
+//Player's selection
+function playerPlay(){
+    do {
+        playerSelection = prompt("what are you picking ?");
+        playerSelection = playerSelection.toLowerCase();
+    }
+    while (playerSelection != "rock" && playerSelection != "paper" && playerSelection != "scissors" );
+    return playerSelection;
+}
+
+//Randomly generates computer selection
 function computerPlay(){
     let item = options[Math.floor(Math.random()* options.length)];
     let computerSelection = item;
@@ -9,80 +25,43 @@ function computerPlay(){
     return computerSelection;
 }
 
-let playerScore = 0;
-let computerScore = 0;
-
-function playRound(playerInput){
-    let computerSelection = computerPlay();
-    let playerSelectionString = String(playerInput);
-    let playerSelection = playerSelectionString.toLowerCase();
-    
-    console.log(computerSelection);
-    console.log(playerSelection);
-
-    switch(playerSelection){
-        case 'rock':
-            if (computerSelection == 'rock'){
-                playerScore++;
-                computerScore++;
-                console.log(playerScore);
-                console.log(computerScore);
-                return 'TIE';
-            } else if (computerSelection == 'paper'){
-                computerScore++;
-                console.log(playerScore);
-                console.log(computerScore);
-                return 'LOSE';
-            } else {
-                playerScore++;
-                console.log(playerScore);
-                console.log(computerScore);
-                return 'WIN'
-            }
-            break;
-        case 'paper':
-            if (computerSelection == 'paper'){
-                playerScore++;
-                computerScore++;
-                console.log(playerScore);
-                console.log(computerScore);
-                return 'TIE';
-            } else if (computerSelection == 'scissors'){
-                computerScore++;
-                console.log(computerScore);
-                console.log(computerScore);
-                return 'LOSE';
-            } else {
-                playerScore++;
-                console.log(playerScore);
-                console.log(computerScore);
-                return 'WIN'
-            }
-            break;
-        case 'scissors':
-            if (computerSelection == 'scissors'){
-                playerScore++;
-                computerScore++;
-                console.log(playerScore);
-                console.log(computerScore);
-                return 'TIE';
-            } else if (computerSelection == 'rock'){
-                computerScore++;
-                console.log(playerScore);
-                console.log(computerScore);
-                return 'LOSE';
-            } else {
-                playerScore++;
-                console.log(playerScore);
-                console.log(computerScore);
-                return 'WIN'
-            }
-            break;   
+//Determines the winner by using the game's rules.
+function playRound(computerSelection, playerSelection){
+    if (computerSelection == playerSelection){
+        // win++;
+        // lose++;
+        console.log(win, lose);
+        console.log(`You tied, the computer picked ${playerSelection} aswell.
+        current score: you ${win} vs computer ${lose}`);
+    } else if((computerSelection == 'rock' && playerSelection == 'paper')||(computerSelection == 'paper' && playerSelection =='scissors')||(computerSelection=='scissors' && playerSelection=='rock')){
+        win++;
+        console.log(`You won, the computer picked ${computerSelection} and you picked ${playerSelection}.
+        current score: you ${win} vs computer ${lose}`);
+    } else{
+        lose++;
+        console.log(`You lost, the computer picked ${computerSelection} and you picked ${playerSelection}.
+        current score: you ${win} vs computer ${lose}`);
     }
+    return ;
+    //win, lose;
 }
 
-// function game(){
-//     for (let i = 0; i < 5; i++){
-//         playRound('Rock');
-//     }
-// }
+// Plays a round of the game until either player reaches 5 wins. 
+function game(){
+    // let win =0;
+    // let lose =0;
+    let winner = '';
+    while (win + lose <= 10){
+        if (win == 5 || lose == 5 ){
+            if (win == 5){
+                winner = 'Player';
+            } else{
+                winner = 'Computer';
+            }
+            console.log(`The winner is ${winner} with a score of computer ${lose} VS player ${win}`);
+            break;
+        }
+        playRound(computerPlay(), playerPlay());
+
+    }
+}
